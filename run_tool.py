8abec86f8f22572dc8536d6fe79c20fe6e74cd95
dnash <dnash@skyriver.ucsd.edu>
2023-10-27 07:53:21 -0700

    remove font requirement

--- run_tool.py
+++ run_tool.py
@@ -10,9 +10,9 @@ from ar_landfall_tool_contour import landfall_tool_contour
 from ar_landfall_tool_vector import landfall_tool_vector
 from ar_landfall_tool_IVT_mag import landfall_tool_IVT_magnitude
 from datetime import datetime
-from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['PT Sans']})
-rc('text', usetex=True)
+# from matplotlib import rc
+# rc('font',**{'family':'sans-serif','sans-serif':['PT Sans']})
+# rc('text', usetex=True)
 
 
 startTime = datetime.now() # get start time of script
